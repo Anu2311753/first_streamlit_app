927f9a3b805315b2a4a1a73e73d73cd08eb2e4d8
Anurag Pandey <120084276+Anu2311753@users.noreply.github.com>
2022-12-08 13:03:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,4 @@ import streamlit
 streamlit.title('My Parents New Healthy Dinnerr')
 streamlit.header('Breakfast menu is ready') 
 streamlit.text('omega tree and oatmeal omlete')
+streamlit.text('author have to take a large plate')
